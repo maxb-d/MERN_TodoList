1d6b4d856ceb33c98eddbcb1eaea62b60b881e00
Maxime Baude <maxime.baude0@gmail.com>
2023-01-26 15:44:02 +0100

    Add simple frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+
+import { FaTrash } from 'react-icons/fa'
+
+const API_BASE = 'http://localhost:3001'
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const [newTodo, setNewTodo] = useState('')
+
+  useEffect(() => {
+    getTodos()
+
+    console.log(todos)
+  }, [])
+
+  const getTodos = async () => {
+    const data = await fetch(API_BASE + "/todos")
+      .then(result => result.json())
+      .then(data => setTodos(data))
+      .catch(err => console.error('Error: ', err))
+  }
+
+  const completeTodo = async (id) => {
+    const data = await fetch(API_BASE + '/todo/complete/' + id, { method: 'PATCH' })
+      .then(res => res.json())
+
+      setTodos(todos => todos.map(todo => {
+        if(todo._id === data._id) {
+          todo.complete = data.complete
+        }
+
+        return todo
+      }))
+  }
+
+  const deleteTodo = async (id) => {
+    const data = await fetch(API_BASE + '/todo/delete/' + id, { method: 'DELETE' })
+      .then(res => res.json())
+
+    setTodos(todos => todos.filter(todo => todo._id !== data._id))
+  }
+
+  const addTodo = async () => {
+    const data = await fetch(API_BASE + '/todo/new', { 
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json",
+      }, 
+      body: JSON.stringify({
+        text: newTodo
+      })
+    }).then(res => res.json())
+
+    setTodos([...todos, data])
+    setNewTodo('')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Awesome To-Do List</h1>
+      <h4>Here are your tasks for the day</h4>
+      <div className='todos'>
+        {todos.map(todo => (
+          <div className='todo' key={todo._id} onClick={() => completeTodo(todo._id)}>
+            <div className={`checkbox ${todo.complete ? "is-complete" : ""}`}></div>
+            <p className={`todo--content ${todo.complete ? "is-complete" : ""}`}>{todo.text}</p>
+            <button className='todo--trash' onClick={(e) => {e.stopPropagation(); deleteTodo(todo._id)}}><FaTrash size={25} /></button>
+          </div>
+        ))}
+
+        <div className='add--todo'>
+          <div className='todo'>
+            <p className={`todo--content `}>
+              <input 
+                type="text"
+                className='new--todo-content'
+                placeholder='Enter a new todo'
+                onChange={e => setNewTodo(e.target.value)}
+                value={newTodo}
+              ></input>
+            </p>
+          </div>
+          <button className='todo--add' onClick={addTodo}>+</button>
+        </div>
+      </div>
+
     </div>
   );
 }
